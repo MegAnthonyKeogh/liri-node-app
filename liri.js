aa0b3f3984bd484f3b19af4e2355b264cfd94d12
Megan Anthony <megananthony@Megans-MacBook-Air.local>
2018-07-19 14:50:42 -0400

    finishing up

--- liri.js
+++ liri.js
@@ -1,4 +1,3 @@
-
 require("dotenv").config();
 var keys = require('./keys.js');
 var Twitter = require('twitter');
@@ -7,67 +6,87 @@ var Spotify = require('node-spotify-api');
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
 var request = require("request");
-var movie = process.argv[2];
-  
-// function tweets(){
-//  var params = 'MommyValidated';
-//  client.get('search/tweets', {q: params}, function(error, tweets, response) {
-//      if (error){
-//          return console.log(error)
-//      }
-//       if (!error) {
-//         for (var i = 0; i < 20; i++){
-//            console.log("tweet created at " + tweets.statuses[i].created_at);
-//            console.log("tweet said " + tweets.statuses[i].text)
-//        //console.log(tweets);
-//       }
-//     }
-// });
-// }
-// function playSong(){
-// var songName = "";
-// var args = process.argv
-//  for (var i = 3; i < args.length; i++) {
-//      songName = songName + " " + args[i];
-//  } console.log("SongName" + songName);
- 
-// spotify.search({ type: 'track', query: songName }, function(err, data) {
-//   if (err) {
-//     return console.log('Error occurred: ' + err);
-//   }
- 
-// console.log("album name " + data.tracks.items[i].album.name  );
-// console.log("song name "+ data.tracks.items[i].name );
-// console.log("preview link " + data.tracks.items[i].preview_url);
- 
-// });
-// }
 
-// Basic Node application for requesting data from the OMDB website
-// Here we incorporate the "request" npm package
+//twitter call 
+function tweets(){
+ var params = 'MommyValidated';
+ client.get('search/tweets', {q: params}, function(error, tweets, response) {
+     if (error){
+         return console.log(error)
+     }
+      if (!error) {
+        for (var i = 0; i < 20; i++){
+           console.log("tweet created at " + tweets.statuses[i].created_at);
+           console.log("tweet said " + tweets.statuses[i].text)
+       //console.log(tweets);
+      }
+    }
+});
+}
+//spotify call
+function playSong() {
+    var songName = "";
+    var args = process.argv
+    for (var i = 3; i < args.length; i++) {
+        songName = songName + " " + args[i];
+    }
+    console.log("SongName" + songName);
+
+    spotify.search({
+        type: 'track',
+        query: songName
+    }, function (err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+        console.log(data.tracks.items[0].artists[0].name);
+        console.log("album name " + data.tracks.items[0].album.name);
+        console.log("song name " + data.tracks.items[0].name);
+        console.log("preview link " + data.tracks.items[0].preview_url);
 
 
-// // We then run the request module on a URL with a JSON
-// request("http://www.omdbapi.com/?t=" + movie + "&apikey=trilogy", function(error, response, body) {
+    });
 
-//   // If there were no errors and the response code was 200 (i.e. the request was successful)...
-//   if (!error && response.statusCode === 200) {
+}
 
-//     // Then we print out the imdbRating
-//     console.log("The movie's rating is: " + JSON.parse(body).imdbRating);
-//   }
-// });
+//omdb call
+function movieRequest() {
+    var movie = "";
+    var title = process.argv
+    for (var i = 3; i < title.length; i++) {
+        movie = movie + " " + title[i];
+    }
+    console.log("The movie's name is" + movie);
+
+    // We then run the request module on a URL with a JSON
+    request("http://www.omdbapi.com/?t=" + movie + "&apikey=trilogy", function (error, response, body) {
+        var parseBody = JSON.parse(body);
+        // If there were no errors and the response code was 200 (i.e. the request was successful)...
+        if (!error && response.statusCode === 200) {
+            //ratings.length
+            console.log("Title is " + parseBody.Title);
+            console.log("The country is:" + parseBody.Country);
+            console.log("The plot is:" + parseBody.Plot);
+            console.log("The year this movie came out:" + parseBody.Year);
+            console.log("The rating was:" + parseBody.imdbRating);
+            console.log("The actors were " + parseBody.Actors);
+            console.log("I love watching this movie in " + parseBody.Language);
+            console.log("Rotten Tomatoes Rating was: " + JSON.parse(body).Ratings[1].Value);
+        }
+    });
+}
 
 //actual code 
 if (process.argv[2] === "my-tweets") {
     tweets();
     console.log("go to twitter");
-} else if (process.argv[2] === "spotify-this-song") { 
+} else if (process.argv[2] === "spotify-this-song") {
     playSong();
     console.log("got to spotify");
-} else if (process.argv[2] === "movie-this"){
+} else if (process.argv[2] === "movie-this") {
+    movieRequest();
     console.log("OMDB");
-} else if (process.argv[2]=== "do-what-it-says"){
+} else if (process.argv[2] === "do-what-it-says") {
     console.log("just do it");
 } else {
     console.log("I don't know what you want");
